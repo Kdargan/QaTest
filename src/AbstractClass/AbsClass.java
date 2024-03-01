c5e552278ae6d25e94f03b5be96e8072b576786a
kdargan <kirandeepdargan@gmail.com>
2024-03-02 01:43:05 +0530

    update 1 file

--- src/AbstractClass/AbsClass.java
+++ src/AbstractClass/AbsClass.java
@@ -4,7 +4,7 @@ public abstract class AbsClass extends AbsPart {
 	
 	
 	public void loginAWS() {
-		System.out.println("Print login to AWS");
+		System.out.println("Print login 2 AWS");
 	}
 	public abstract void openAws();
 
